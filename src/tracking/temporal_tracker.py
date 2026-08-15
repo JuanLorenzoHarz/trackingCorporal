e878908315cd23737948b7d55ac8fc41e6463d88
JuanLorenzoHarz <juanlorenzoharz@gmail.com>
2026-08-15 01:31:40 -0300

    chore: add temporal tracker placeholder

---
+++ src/tracking/temporal_tracker.py
@@ -0,0 +1,7 @@
+"""Associação temporal da pose entre frames consecutivos.
+
+Responsabilidade futura:
+- manter estado da pose anterior;
+- lidar com keypoints temporariamente perdidos;
+- fornecer histórico mínimo para estabilização e análise de movimento.
+"""
