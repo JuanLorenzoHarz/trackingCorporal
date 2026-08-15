97a28074a3262aa157e8f3c2aac95873a166728e
JuanLorenzoHarz <juanlorenzoharz@gmail.com>
2026-08-15 01:32:21 -0300

    chore: add config module placeholder

---
+++ src/core/config.py
@@ -0,0 +1,7 @@
+"""Carregamento e validação das configurações do projeto.
+
+Responsabilidade futura:
+- ler o arquivo YAML;
+- fornecer configurações tipadas aos módulos;
+- validar valores obrigatórios e limites básicos.
+"""
