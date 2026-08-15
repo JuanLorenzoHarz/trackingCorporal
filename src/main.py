9fe003df889a62dcd36d8f1029a0f0074f71a74a
JuanLorenzoHarz <juanlorenzoharz@gmail.com>
2026-08-15 01:30:55 -0300

    chore: add application entry point placeholder

---
+++ src/main.py
@@ -0,0 +1,9 @@
+"""Ponto de entrada do trackingCorporal.
+
+Responsabilidade futura:
+- carregar configuração;
+- iniciar a câmera;
+- coordenar o pipeline de pose, tracking, mãos e visualização.
+
+A implementação será adicionada em uma etapa posterior.
+"""
