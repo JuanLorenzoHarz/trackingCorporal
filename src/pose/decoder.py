d725c1f9169d09a16b574c708862486415992b19
JuanLorenzoHarz <juanlorenzoharz@gmail.com>
2026-08-15 01:31:31 -0300

    chore: add pose decoder placeholder

---
+++ src/pose/decoder.py
@@ -0,0 +1,7 @@
+"""Decodificação da saída do modelo de pose.
+
+Responsabilidade futura:
+- converter heatmaps em coordenadas de keypoints;
+- calcular confiança por ponto;
+- remapear coordenadas para a resolução original do frame.
+"""
