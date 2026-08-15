b5ebf05f33cf80692e8e1e398a3de4e5032213ee
JuanLorenzoHarz <juanlorenzoharz@gmail.com>
2026-08-15 01:32:26 -0300

    chore: add shared types placeholder

---
+++ src/core/types.py
@@ -0,0 +1,8 @@
+"""Tipos de dados compartilhados pelo pipeline.
+
+Responsabilidade futura:
+- representar keypoints e poses;
+- representar níveis de confiança;
+- representar estados das mãos;
+- evitar estruturas de dados improvisadas entre módulos.
+"""
