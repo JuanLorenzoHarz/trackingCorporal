30f7ec113b4fe0556e87be79642868b1e8568d23
JuanLorenzoHarz <juanlorenzoharz@gmail.com>
2026-08-15 01:31:54 -0300

    chore: add hand ROI placeholder

---
+++ src/hands/hand_roi.py
@@ -0,0 +1,7 @@
+"""Localização da região de interesse de cada mão.
+
+Responsabilidade futura:
+- estimar a área da mão a partir de punho, cotovelo e escala corporal;
+- recortar a ROI sem executar um detector completo de mãos;
+- informar quando a região não for confiável.
+"""
