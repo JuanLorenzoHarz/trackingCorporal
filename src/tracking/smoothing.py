fabd3bb66c325decd0294c568f95f119054f15f3
JuanLorenzoHarz <juanlorenzoharz@gmail.com>
2026-08-15 01:31:45 -0300

    chore: add smoothing placeholder

---
+++ src/tracking/smoothing.py
@@ -0,0 +1,7 @@
+"""Filtros de suavização para reduzir tremores nos keypoints.
+
+Responsabilidade futura:
+- aplicar suavização temporal às coordenadas;
+- preservar movimentos rápidos sem introduzir atraso excessivo;
+- permitir comparação entre diferentes filtros.
+"""
