99d454f1c394e627649c9387996fcc99d8fc8528
JuanLorenzoHarz <juanlorenzoharz@gmail.com>
2026-08-15 01:31:27 -0300

    chore: add pose model placeholder

---
+++ src/pose/model.py
@@ -0,0 +1,7 @@
+"""Arquitetura do modelo de estimativa de pose.
+
+Responsabilidade futura:
+- definir a CNN em PyTorch;
+- produzir heatmaps ou outra representação intermediária dos keypoints;
+- manter a arquitetura separada das rotinas de treino e inferência.
+"""
