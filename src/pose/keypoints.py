f6139c84df573f184a73d6bdc453b1035a8e8e4d
JuanLorenzoHarz <juanlorenzoharz@gmail.com>
2026-08-15 01:31:22 -0300

    chore: add keypoint definitions placeholder

---
+++ src/pose/keypoints.py
@@ -0,0 +1,7 @@
+"""Definição do esqueleto e dos keypoints corporais.
+
+Responsabilidade futura:
+- listar os aproximadamente 17 pontos corporais usados pelo projeto;
+- definir conexões entre articulações para formar o esqueleto;
+- concentrar índices e nomes para evitar números mágicos espalhados pelo código.
+"""
