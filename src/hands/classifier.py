3780e69c6f6261bd8cc4f05bc790fb19909b9ff9
JuanLorenzoHarz <juanlorenzoharz@gmail.com>
2026-08-15 01:32:00 -0300

    chore: add hand classifier placeholder

---
+++ src/hands/classifier.py
@@ -0,0 +1,7 @@
+"""Classificação simples do estado das mãos.
+
+Responsabilidade futura:
+- receber uma ROI de mão;
+- classificar o estado como OPEN, CLOSED ou UNKNOWN;
+- retornar confiança da classificação.
+"""
